fa1f1ac6b46d580eac317ddc392209f54ac79317
Calvin Davis <hi@calvindavis.co.uk>
2014-06-02 22:22:26 +0100

    Implemented basic image serving!

--- web.js
+++ web.js
@@ -1,17 +1,50 @@
 var express = require("express");
+var fs = require("fs");
+var gm = require("gm");
+var im = gm.subClass({ imageMagick: true });
+
 var app = express();
 var port = Number(process.env.PORT || 5000);
 
 app.use(express.static("static"));
 
-app.get("/json", function (req, res) {
-	var result = {
-		placehog: true
-	};
-	var message = JSON.stringify(result);
+var getImageName = (function () {
+	var imageNames = [
+		"image-1",
+		"image-2",
+		"image-3"
+	];
 	
-	res.set("Content-Type", "application/json");
-	res.send(message);
-});
+	return function (width, height) {
+		var size = width + height;
+		var index = size % imageNames.length;
+		var imageName = imageNames[index];
+		
+		return imageName;
+	};
+})();
+
+var requestHandler = function (req, res) {
+	var params = req.params;
+	var width = Number(params.width) || 100;
+	var height = Number(params.height) || width;
+	var imageName = getImageName(width, height);
+	var imagePath = "./images/" + imageName + ".jpg";
+	var cachePath = "./cache/" + imageName + "-" + width + "x" + height + ".jpg";
+	var image = null;
+	
+	if (fs.existsSync(cachePath)) {
+		res.sendfile(cachePath);
+	} else {
+		image = im(imagePath);
+		image.resize(width, height);
+		image.write(cachePath, function (err) {
+			res.sendfile(cachePath);
+		});
+	}
+};
+
+app.get("/:width", requestHandler);
+app.get("/:width/:height", requestHandler);
 
 app.listen(port);
